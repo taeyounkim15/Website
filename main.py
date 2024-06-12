6d3aa2afedf9f1eb4c96f868a651b49ac8b12733
tune <taeyounkim15@gmail.com>
2024-06-12 15:08:19 +0700

    Make a dropdown of Bond Code

--- main.py
+++ main.py
@@ -3,28 +3,26 @@ from google.cloud import datastore
 from datetime import datetime, timezone
 import gspread
 from google.oauth2.service_account import Credentials
+import pandas as pd
 
 # ============================
-# Let's try to bring in the data from the Spreadsheet
-CREDENTIALS_FILE = 'credentials.json'
+# # Let's try to bring in the data from the Spreadsheet
+# CREDENTIALS_FILE = 'credentials.json'
 
-# Define the scope and authenticate
-SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPE)
-client = gspread.authorize(creds)
+# # Define the scope and authenticate
+# SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+# creds = Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPE)
+# client = gspread.authorize(creds)
 
-# Google Spreadsheet ID and sheet name
-SPREADSHEET_ID = '1hEfWYWhbnfN3uURJTQNaDV3QAEEyMzKtmV888E0fA8M'
-SHEET_NAME = 'DB'
+# # Google Spreadsheet ID and sheet name
+# SPREADSHEET_ID = '1hEfWYWhbnfN3uURJTQNaDV3QAEEyMzKtmV888E0fA8M'
+# SHEET_NAME = 'DB'
 
-# Open the spreadsheet by ID and sheet by name
-spreadsheet = client.open_by_key(SPREADSHEET_ID)
-worksheet = spreadsheet.worksheet(SHEET_NAME)
+# # Open the spreadsheet by ID and sheet by name
+# spreadsheet = client.open_by_key(SPREADSHEET_ID)
+# worksheet = spreadsheet.worksheet(SHEET_NAME)
 
-data = worksheet.get_all_values()
-print(data[0
-
-])
+# data = worksheet.get_all_values()
 
 # ============================
 
@@ -33,30 +31,48 @@ datastore_client = datastore.Client()
 def store_time(dt):
     entity = datastore.Entity(key=datastore_client.key("visit"))
     entity.update({"timestamp": dt})
-
     datastore_client.put(entity)
-
 
 def fetch_times(limit):
     query = datastore_client.query(kind="visit")
     query.order = ["-timestamp"]
-
     times = query.fetch(limit=limit)
-
     return times
-
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    # ============================
+    # Let's try to bring in the data from the Spreadsheet
+    CREDENTIALS_FILE = 'credentials.json'
+
+    # Define the scope and authenticate
+    SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPE)
+    client = gspread.authorize(creds)
+
+    # Google Spreadsheet ID and sheet name
+    SPREADSHEET_ID = '1hEfWYWhbnfN3uURJTQNaDV3QAEEyMzKtmV888E0fA8M'
+    SHEET_NAME = 'DB'
+
+    # Open the spreadsheet by ID and sheet by name
+    spreadsheet = client.open_by_key(SPREADSHEET_ID)
+    worksheet = spreadsheet.worksheet(SHEET_NAME)
+
+    data = worksheet.get_all_values()
+    df = pd.DataFrame(data[1:], columns=data[0])
+
+    # print(data[0])
+    # ============================
+
+
     # Store the current access time in Datastore.
-    store_time(datetime.now(tz=timezone.utc))
+    # store_time(datetime.now(tz=timezone.utc))
 
     # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
-    return render_template("home.html", times=times)
+    # times = fetch_times(10)
+    return render_template("home.html", codes=df["Code"])
     
 @app.route("/about")
 def about():
